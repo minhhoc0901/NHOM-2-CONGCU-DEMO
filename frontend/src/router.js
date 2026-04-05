51ca7dd390cca49f878d0fb77821de211ce8254e
vanivietquat <dinhhuy25102004@gmail.com>
2026-04-05 12:11:53 +0700

    Update Ui Auth

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LocationListPage from './pages/LocationListPage';
 import LocationDetailPage from './pages/LocationDetailPage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
+import ProfilePage from './pages/auth/ProfilePage';
+import PrivateRoute from './components/auth/PrivateRoute';
 
 
 const AppRouter = () => {
@@ -11,6 +16,17 @@ const AppRouter = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/locations" element={<LocationListPage />} />
             <Route path="/locations/:id" element={<LocationDetailPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route
+                path="/profile"
+                element={
+                    <PrivateRoute>
+                        <ProfilePage />
+                    </PrivateRoute>
+                }
+            />
         </Routes>
     );
 };
